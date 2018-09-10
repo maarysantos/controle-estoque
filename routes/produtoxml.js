e8efe7de7bd2f60ee04b75c158b0d91890712aab
MaarySantos <maary.santos1@gmail.com>
2018-09-10 20:30:22 -0300

    Lendo xml

--- routes/produtoxml.js
+++ routes/produtoxml.js
@@ -2,52 +2,37 @@ var express = require('express');
 var router = express.Router();
 var fs           = require('fs');
 var xml2js       = require('xml2js');
-var parser       = new xml2js.Parser();
 var multer  = require('multer');
-var upload = multer({storage });
 
-var storage = multer.diskStorage({
+
+//Salvando a nota com o nome original
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+      cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+      cb(null, file.originalname);
   }
-})
- 
-var upload = multer({ storage: storage })
+});
 
-
+var upload = multer({storage});
 
 
 router.post('/', upload.single('upXml'), function(req, res, next) {
-  var xml= req.file;
-  console.log(xml);
+  var fileDate = fs.readFileSync(multer, 'ascii');
+  var parser       = new xml2js.Parser();
 
-  fs.readFile("uploads/"+xml.filename, "utf-8", function (error, text) {
-    if (error) {
-
-      throw error;
-
-  }else {
-
-      parser.parseString(text, function (err, result) {
-
-       
-          console.log(result);
-        
-
-        // res.render('produtoxml', { produtos:  produtos });
-    
-      });
-
-  }
-
+  parser.parseString(fileDate.substring(0, fileDate.length), function(err, result){
+    var [ notaFiscal ] = result.nfeProc.NFe;// primeira tag nfe
+    var [ informacao ] = notaFiscal.infNFe;// infnfe
+   res.render('produtoxml', {informacao});
 });
-});
+   
+  });
+
+
 
 router.get('/', function(req, res, next) {
-  res.render('produtoxml', { });
-});
 
+});
 module.exports = router;
