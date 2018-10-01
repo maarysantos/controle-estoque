eb79cb8c96fb31e094663956bf583eecb1607788
MaarySantos <maary.santos1@gmail.com>
2018-10-01 12:16:11 -0300

    Mostrar dados do produto selecionado para edição

--- routes/saidaproduto.js
+++ routes/saidaproduto.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('saidaproduto');
@@ -8,13 +9,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/search',function(req,res){
 var connection = db();
-connection.query('SELECT ds_produto from estoque where ds_produto like "%'+req.query.key+'%"',
+connection.query('SELECT * from estoque where cd_produto like "%'+req.query.key+'%" or ds_produto like "%'+req.query.key+'%"',
 function(err, rows, fields) {
 if (err) throw err;
 var data=[];
 for(i=0;i<rows.length;i++)
 {
 data.push(rows[i].ds_produto);
+
 }
 res.end(JSON.stringify(data));
 });
@@ -22,5 +24,24 @@ res.end(JSON.stringify(data));
 
 });
 
+router.get('/order', function(req, res, next) {
+  var connection = db();
+  var nome= req.param;                                                
+connection.query('SELECT * from estoque where cd_produto like "%'+nome+'%" or ds_produto like "%'+nome+'%"',
+function(err, rows, fields) {
+if (err) throw err;
+var data=[];
+for(i=0;i<rows.length;i++)
+{
+data.push(rows[i].cd_produto);
+data.push(rows[i].ds_produto);
+data.push(rows[i].qt_produto);
+
+}
+res.send(data );
+});
+
+
+});
 
 module.exports = router;
