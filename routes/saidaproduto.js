46a2707935173204dc67232600187d9902325fdd
nenodias <horacio.dias92@gmail.com>
2018-10-01 20:18:14 +0000

    Comitando as alterações

--- routes/saidaproduto.js
+++ routes/saidaproduto.js
@@ -8,39 +8,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/search',function(req,res){
-var connection = db();
-connection.query('SELECT * from estoque where cd_produto like "%'+req.query.key+'%" or ds_produto like "%'+req.query.key+'%"',
-function(err, rows, fields) {
-if (err) throw err;
-var data=[];
-for(i=0;i<rows.length;i++)
-{
-data.push(rows[i].ds_produto);
+    var connection = db();
+    connection.query('SELECT * from estoque where cd_produto like "%'+req.query.key+'%" or ds_produto like "%'+req.query.key+'%"',
+        function(err, rows, fields) {
+            if (err) throw err;
+            var data=[];
+            for(i=0;i<rows.length;i++)
+            {
+                let { cd_produto, ds_produto, qt_produto } = rows[i];
+                data.push({ cd_produto, ds_produto, qt_produto });
 
-}
-res.end(JSON.stringify(data));
-});
-
-
+            }
+            res.end(JSON.stringify(data));
+        });
 });
 
 router.get('/order', function(req, res, next) {
   var connection = db();
-  var nome= req.param;                                                
-connection.query('SELECT * from estoque where cd_produto like "%'+nome+'%" or ds_produto like "%'+nome+'%"',
-function(err, rows, fields) {
-if (err) throw err;
-var data=[];
-for(i=0;i<rows.length;i++)
-{
-data.push(rows[i].cd_produto);
-data.push(rows[i].ds_produto);
-data.push(rows[i].qt_produto);
-
-}
-res.send(data );
-});
-
+  var nome= req.param;
+  connection.query('SELECT * from estoque where cd_produto like "%'+nome+'%" or ds_produto like "%'+nome+'%"',
+  function(err, rows, fields) {
+      if (err) throw err;
+      var data=[];
+      for(i=0;i<rows.length;i++)
+      {
+          let { cd_produto, ds_produto, qt_produto } = rows[i];
+          data.push({ cd_produto, ds_produto, qt_produto });
+      }
+      res.send(data );
+  });
 
 });
 
