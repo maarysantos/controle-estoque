bda07c0451ce83636e0dcefda3327834938b10be
MaarySantos <maary.santos1@gmail.com>
2018-10-04 10:48:13 -0300

    falhado

--- routes/atualizarEstoque.js
+++ routes/atualizarEstoque.js
@@ -13,9 +13,10 @@ router.post('/', function(req, res) {
         else{
             var qt_saida = result[0].qt_produto;
             var res= qt_saida - quantidade;
-            if( vl_baixo - quantidade >= 0){
-               // connection.query('UPDATE collegeusers SET printCount = ? WHERE userid = ?', [printCount, userid], function (error, results, fields) {
-                connection.query("UPDATE estoque set qt_produto = qt_produto -"+ quantidade + "where cd_produto="+codigo+"",
+            console.log(res);
+            if( res >= 0){
+                
+                connection.query("UPDATE estoque set qt_produto=:Saida where cd_produto=:Codigo", {Saida: qt_saida, Codigo:codigo },
                 function(error, result){
                 if (error) throw error;
                                    console.log("Atualizado!");
