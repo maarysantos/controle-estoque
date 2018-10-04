280e5a2a922d56c1bca1830f5316b57ee4558790
MaarySantos <maary.santos1@gmail.com>
2018-10-03 21:25:03 -0300

    .

--- routes/atualizarEstoque.js
+++ routes/atualizarEstoque.js
@@ -9,21 +9,18 @@ router.post('/', function(req, res) {
     var quantidade = req.body.qt_produto;
 
     connection.query("SELECT qt_produto from estoque where cd_produto ="+codigo+"", function(error, result) {
-        if (error){ console.log(error);}
-        else{
-            var qt_saida = result[0].qt_produto;
-            var res= qt_saida - quantidade;
-            if( vl_baixo - quantidade >= 0){
-               // connection.query('UPDATE collegeusers SET printCount = ? WHERE userid = ?', [printCount, userid], function (error, results, fields) {
-                connection.query("UPDATE estoque set qt_produto = qt_produto -"+ quantidade + "where cd_produto="+codigo+"",
-                function(error, result){
-                if (error) throw error;
-                                   console.log("Atualizado!");
+    if (error){ console.log(error);}
+ 
+    var qt_produto = result[0].qt_produto;
+    var res = qt_produto - quantidade;
+    
+    connection.query('UPDATE estoque SET qt_produto = ? WHERE cd_produto = ?', [res, codigo], function (error, results, fields) {
+        if (error) throw error;
+       console.log('ola')
+      });                        
+           
                                 });
                
-        }
-    }
-                   
-                            });  
-                        });  
+                            });
+                       
 module.exports = router;
