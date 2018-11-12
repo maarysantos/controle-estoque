1c657cbcc0eb4c65e50d3014516dd4a0848f9628
nenodias <horacio.dias92@gmail.com>
2018-11-12 18:25:09 +0000

    Atualizando o layout com ejs do menu

--- server/routes/fornecedor.js
+++ server/routes/fornecedor.js
@@ -5,10 +5,10 @@ var controller = require('../controllers/fornecedor');
 router.get('/novofornecedor', controller.getNovoFornecedor);
 router.post('/novofornecedor', controller.insereNovoFornecedor);
 
+router.get('/listafornecedores/delete/:id', controller.deleteFornecedor);
+router.get('/listafornecedores/edit/:id', controller.editarFornecedor);
 router.get('/listafornecedores', controller.listaFornecedores);
-router.get('/delete/:id', controller.deleteFornecedor);
 
-router.get('/edit/:id', controller.editarFornecedor);
 
   
 module.exports = router;
